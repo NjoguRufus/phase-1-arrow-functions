6be67bd924255ab046e136e0c8b37c15fc15e873
NjoguRufus <rufus.njogu@student.moringaschool.com>
2024-03-24 12:53:08 +0300

    arrow functions

---
+++ index.js
@@ -0,0 +1,19 @@
+// Function expression divide
+const divide = function(x, y) {
+    return x / y;
+};
+
+// Arrow function square
+const square = (x) => {
+    return x * x;
+};
+
+// Arrow function called add
+const add = (x, y) => {
+    return x + y;
+};
+
+//Testing
+console.log(divide(2000, 100)); // Output: 20
+console.log(square(5)); // Output: 25
+console.log(add(3, 7)); // Output: 10
